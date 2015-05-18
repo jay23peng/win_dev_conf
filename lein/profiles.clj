2f8526e41d21067d4735440bf4520c5839abd605
Jesse Peng <pengwenjia@gmail.com>
2015-05-17 21:08:57 -0400

    Lein profile

---
+++ lein/profiles.clj
@@ -0,0 +1,4 @@
+{:user {:plugins 
+        [[cider/cider-nrepl "0.9.0-SNAPSHOT" :exclusions [org.clojure/clojure]]]
+        :dependencies [ [org.clojure/clojure "1.7.0-beta3"]
+                        [org.clojure/tools.nrepl "0.2.10"]]}}
