4bbf3670697699fc37ae9af62262dcfa52489e8b
Wenjia Peng <pengwenjia@gmail.com>
2017-11-25 22:26:15 -0500

    sync to latest

--- windows/with.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-for /f "usebackq delims=" %%i in (`fzf`) do set SELECTION=%%~fi
-if not defined ConEmuHWND goto EXECMD
-start %1 %SELECTION% -cur_console:n
-goto END
-:EXECMD
-start  %1 %SELECTION% 
-:END
