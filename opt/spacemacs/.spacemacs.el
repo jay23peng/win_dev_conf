9408218cf9dafe2645f38450e2bdb5f931dc709f
jesse23 <jsp23@qq.com>
2020-12-30 06:54:07 -0500

    syntax highlight

--- opt/spacemacs/.spacemacs.el
+++ opt/spacemacs/.spacemacs.el
@@ -1,3 +1,4 @@
+;; TODO: rename it back to `.spacemacs` if u really need to use it
 ;; -*- mode: emacs-lisp; lexical-binding: t -*-
 ;; This file is loaded by Spacemacs at startup.
 ;; It must be stored in your home directory.
