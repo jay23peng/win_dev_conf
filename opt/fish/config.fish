c819e97860a5c963dd3b6be015deba8b7be400b7
jesse23 <vijcp@outlook.com>
2021-01-23 23:26:22 -0500

    sync

--- opt/fish/config.fish
+++ opt/fish/config.fish
@@ -10,8 +10,8 @@ set -g -x FZF_DEFAULT_OPTS '--bind=tab:up,btab:down,ctrl-u:page-up,ctrl-d:page-d
 set -g -x EDITOR '/Users/jesse.peng/u/dex/config/mac/bin/em'
 
 # https://emacs.stackexchange.com/questions/50485/copy-or-paste-between-terminal-emacs-26-and-mac-apps
-set -x LC_ALL "en_US.UTF-8"
-set -x LANG 'en_US.UTF-8'
+# set -x LC_ALL "en_US.UTF-8"
+# set -x LANG 'en_US.UTF-8'
 
 # bobthefish
 # set -g theme_use_abbreviated_branch_name yes
